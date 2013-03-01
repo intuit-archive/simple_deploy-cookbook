ca47f264985414b56d2bb7eb9edb0d5182dcbefd
Brett Weaver <brett@weav.net>
2013-03-01 07:44:19 -0800

    updated exists logic

--- libraries/stack_exists.rb
+++ libraries/stack_exists.rb
@@ -4,8 +4,14 @@ class StackExists
     name              = args[:name]
     environment       = args[:environment]
 
-    `simple_deploy status -e #{environment} -n #{name}`
+    shell 'simple_deploy status -e #{environment} -n #{name}'
+  end
+
+  def shell(cmd, log_level='info')
+    Chef::Log.send log_level, cmd
+    `cmd`
     $?.success?
   end
+
 
 end
