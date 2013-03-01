4690123640c6887dc1d7c9a28f7c977dfd39c411
Brett Weaver <brett@weav.net>
2013-03-01 07:47:05 -0800

    updating exists logic

--- libraries/stack_exists.rb
+++ libraries/stack_exists.rb
@@ -4,7 +4,7 @@ class StackExists
     name              = args[:name]
     environment       = args[:environment]
 
-    shell 'simple_deploy status -e #{environment} -n #{name}'
+    shell "simple_deploy status -e #{environment} -n #{name}"
   end
 
   def shell(cmd, log_level='info')
