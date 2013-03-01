6fd880c5a11ca77b3f88b5d102c7d5327235c9eb
Brett Weaver <brett@weav.net>
2013-03-01 07:45:20 -0800

    adding default to inputs

--- resources/stack.rb
+++ resources/stack.rb
@@ -2,7 +2,7 @@ actions :create
 default_action :create
 
 attribute :environment, :kind_of => String, :default => 'default'
-attribute :inputs, :kind_of => Hash
+attribute :inputs, :kind_of => Hash, :default => {}
 attribute :object_name, :kind_of => String
 attribute :read_outputs_from, :kind_of => [Array, String]
 attribute :template, :kind_of => String, :default => nil
