3c4e5ccdb76cfeee4bba258d883814a32913ace2
Brett Weaver <brett@weav.net>
2013-03-06 06:32:16 -0800

    updated log msg

--- libraries/misc.rb
+++ libraries/misc.rb
@@ -1,7 +1,7 @@
 class Misc
   def self.sleep_to_prevent_exceeding_api_threshold
     x = 3
-    Logger.log "Sleeping #{x} seconds to not exceed Cloud Formation API limit."
+    Logger.log "Backing off fo #{x} seconds to not exceed Cloud Formation API limit."
     sleep x
   end
 end
